5ca43102486a8dda6baf5336b215b870168aa814
zhikai <hello_zhikai@163.com>
2020-10-03 18:02:15 +0800

    test

---
+++ src/HiGit.java
@@ -0,0 +1,11 @@
+/**
+ * @className: HiGit
+ * @description:
+ * @author: duzhikai
+ * @create: 2020-10-03 17:44
+ **/
+public class HiGit {
+    public static void main(String[] args) {
+        System.out.println("welcom git ~");
+    }
+}
